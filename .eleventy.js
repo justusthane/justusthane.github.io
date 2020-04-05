61eb2a4ea014bdf5d4421069ea19602dac25976d
Justus <jg@justus.ws>
2020-04-05 16:43:31 -0400

    fixed annoying gitignore issue with node_modules not being ignored by 11ty caushing issues
    Former-commit-id: a96a814401e50a9cff92808fe089f94eb83efd4e

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,5 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.setTemplateFormats([
     "md",
     "html",
