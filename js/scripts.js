5a3a63976aa3fc5b3071db2c8fc6cf3de77d7a7c
Justus Grunow <jg@justus.ws>
2020-11-10 14:50:37 -0500

    Fixed blinking

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,6 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-/*
-async function blink() {
-  var src = $(" #header .avatar ").attr('src');
-  console.log("BLINK!");
-  $( "#header .avatar" ).attr('src','/images/dadi-avatar_blink.png');
-  await sleep(150);
-  $( "#header .avatar" ).attr('src',src);
-}
-*/
 
 $( ".avatar" ).hover(
   function() {
@@ -19,20 +10,6 @@ $( ".avatar" ).hover(
   }
 );
 
-$( "#header .left" ).hover(
-  function() {
-    $( "#header .avatar" ).attr('src','/images/dadi-avatar_left.png');
-  }, function() {
-    $( "#header .avatar" ).attr('src','/images/dadi-avatar.png');
-  }
-);
-$( "#header .right" ).hover(
-  function() {
-    $( "#header .avatar" ).attr('src','/images/dadi-avatar_right.png');
-  }, function() {
-    $( "#header .avatar" ).attr('src','/images/dadi-avatar.png');
-  }
-);
 
 $( ".u-url.u-uid" ).hover(
   function() {
@@ -72,11 +49,11 @@ $( ' .elsewhere a ').hover(
 
 async function blinktimer() {
   async function blink() {
-    var src = $(" #header .avatar ").attr('src');
+    var src = $(" .avatar ").attr('src');
     console.log("BLINK!");
-    $( "#header .avatar" ).attr('src','/images/dadi-avatar_blink.png');
+    $(" .avatar ").attr('src','/images/dadi-avatar_blink.png');
     await sleep(150);
-    $( "#header .avatar" ).attr('src',src);
+    $(" .avatar ").attr('src',src);
   }
   while (true) {
     var delay = Math.floor((Math.random() * 15000) + 0);
